927fba0eb2558ee495681441f113b0bb89ce663c
interrupter <alex.repin@mail.ru>
2022-07-04 23:53:53 +0300

    change: sorter renamed, fixed few name odds

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = {
-	name: require('./const.js').MODULE_NAME,
-	paths:{
-		controllers:  __dirname + '/controllers',
-		fields: __dirname + '/fields',
-		forms: __dirname + '/forms',
-		locales: __dirname + '/locales',
-	},
-	filter:    require('./lib/filter.js'),
-	search:    require('./lib/search.js'),
-	sorter:    require('./lib/sort.js'),
-	return:    require('./lib/return.js'),
-	pager:     require('./lib/pager.js'),
+    name: require("./const.js").MODULE_NAME,
+    paths: {
+        controllers: __dirname + "/controllers",
+        fields: __dirname + "/fields",
+        forms: __dirname + "/forms",
+        locales: __dirname + "/locales",
+    },
+    filter: require("./lib/filter.js"),
+    search: require("./lib/search.js"),
+    sorter: require("./lib/sorter.js"),
+    return: require("./lib/return.js"),
+    pager: require("./lib/pager.js"),
 };
